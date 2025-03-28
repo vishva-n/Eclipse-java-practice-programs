8f11d98982e44aea77e44b1d1c2a45125253ac59
vishva-n <vishvaanarayanasamy@gmail.com>
2025-03-28 14:48:43 +0530

    new patterns program added

---
+++ src/patterns/patternA.java
@@ -0,0 +1,22 @@
+package patterns;
+
+public class patternA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int row=5;
+		int col=5;
+		for(int i=1;i<=row;i++)
+		{
+			for(int j=1;j<=col;j++)
+			{
+				System.out.print("*"+ " ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
